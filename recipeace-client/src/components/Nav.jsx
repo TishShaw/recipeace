d6c2a2096c32bcf25628e2815ee1c0b7fc989c36
Tishtanya Shaw <Tishtanyaa@icloud.com>
2022-09-27 16:37:07 -0400

    Update Navbar and styling

--- recipeace-client/src/components/Nav.jsx
+++ recipeace-client/src/components/Nav.jsx
@@ -1,35 +1,70 @@
-import {navItems} from '../data/CategoryData';
 import { Link } from 'react-router-dom';
 import {useState} from 'react';
-import { Tabs } from './Tabs';
+import {GoThreeBars} from 'react-icons/go';
+import {AiOutlineClose} from 'react-icons/ai';
 
 const Nav = () => {
-    console.log(navItems);
-    const [activeTab, setActiveTab] = useState('');
-    const [activeTabIndex, setActiveTabIndex] = useState(0);
+    const [open, setOpen] = useState(false)
 
-    const onTabSwitch = (newActiveTab) => {
-        setActiveTab(newActiveTab);
-        let currNavItem = navItems.map((navItem) => navItem.title);
-        let index = currNavItem.findIndex(navEL => newActiveTab ===navEL);
-        console.log(index);
-        console.log(currNavItem)
-        if (index > -1) {
-            setActiveTabIndex(index);
-        } else {
-            setActiveTabIndex(0);
-        }
+    const handleTab = (e) => {
+        e.preventDefault();
+        const activePage = window.location.pathname;
+        const tabs = document.querySelectorAll("li a")
+
+        tabs.forEach(link => {
+                if(link.href.includes(`${activePage}`)){
+                    link.classList.add('active');
+                }else {
+                    link.classList.remove('active');
+                }
+        })
+    }
+
+    const handleOpen = (e) => {
+        e.preventDefault();
+        setOpen(!open)
     }
     return (
-        <div className="navbar w-[100vw] py-6 rounded-t-3xl fixed bottom-0 left-0  shadow-2xl bg-gray-200 drop-shadow">
-            <div className="relative text-xl w-[100vw] flex justify-around">
-            { navItems &&
-                <Tabs list={navItems.map((n, i) =>(
-                    <div key={i}>
-                        <Link to={n.route}> {n.title}</Link>
-                    </div>
-                ))} activeTab={activeTab} onTabSwitch={onTabSwitch} />}
+        <div className='h-full w-full'>
+            <div className="flex flex-col">
+                
+                <div className="md:hidden bg-gold flex justify-between items-center px-6 py-4">
+                    
+                    <h1 className="text-2xl">RECIPEACE</h1>
+                    
+                    <div className="md:hidden">
+                        
+                        <div className="flex flex-col items-center">
+                            {open? <AiOutlineClose onClick={handleOpen} className="bg-white text-2xl cursor-pointer"/> : <GoThreeBars onClick={handleOpen}className="text-2xl cursor-pointer"/> }
+                            
+                        </div>
+                        
+                        
+                        {open &&<div className=' '>
+                            <div className='bg-black/70 fixed w-full h-screen z-10 top-[64px] left-0'></div>
+                            <ul className='right-0 bg-gold w-[300px] h-screen fixed mt-4 pr-6 pl-4 z-10 text-xl translate-x-2 duration-300'>
+                                <li className='my-10'><Link to='/dashboard'>Home</Link></li>
+                                <li className='my-10'><Link to='/profile/:id'>Profile</Link></li>
+                                <li className='my-10'><Link to='/settings'>Settings</Link></li>
+                                <li className='my-10'><Link to='/about-us'>About Us</Link></li>
+                                <li className='my-10'><Link to='/contact-us'>Contact Us</Link></li>
+                            </ul>
+                        </div>}                     
+                    </div>  
+                </div>
             </div>
+            <div className="sm:hidden bg-gold md:flex justify-between items-center px-6 py-4">
+                <h1 className="text-2xl md:text-2xl lg:text-3xl">RECIPEACE</h1>
+                <ul className='flex text-[16px] lg:text-[18px]' onClick={handleTab}>
+                            <li className='mx-2' id="tab"><Link to='/dashboard'>Home</Link></li>
+                            <li className='mx-2' id="tab"><Link to='/profile/:id'>Profile</Link></li>
+                            <li className='mx-2' id="tab"><Link to='/settings'>Settings</Link></li>
+                            <li className='mx-2' id="tab">About Us</li>
+                            <li className='mx-2' id="tab">Contact Us</li>
+                </ul>
+                <button className='bg-black rounded p-2 text-gold text-[16px] lg:text-[18px]'>Add Recipe</button>
+            </div> 
+
         </div>
     );
 };
